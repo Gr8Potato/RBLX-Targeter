dd5f9d8617861447a99fcfd08ad60d101c952be4
Gr8Potato <37940904+Gr8Potato@users.noreply.github.com>
2024-06-04 21:03:42 -0500

    updated targets 04JUN24

--- targeter.py
+++ targeter.py
@@ -1,12 +1,336 @@
+'''
+======================
+By: Gr8P0tat0
+Last Modified: 04JUN24
+======================
+nighthawk_groups = {
+    "Nighthawk Combat Engineers": 4809530,
+    "Nighthawk Combat Engineers: Leviathan": 33391710,
+    "Nighthawk Commandos": 3496996,
+    "Nighthawk Commandos: Ghosts": 11000162,
+    "Nighthawk Guardians": 4183818,
+    "Nighthawk Imperial Peacekeeper Corps": 4543661,
+    "Nighthawk Imperium": 1174414,
+    "Nighthawk Integration Department": 8244715,
+    "Nighthawk Manticore": 15026315,
+    "Nighthawk Military Police": 3497030,
+    "Nighthawk Military Police: Cerberus": 4486074,
+    "Nighthawk Reaper Battalion": 4734688,
+    "Nighthawk Reaper Battalion: Black Daggers": 4936035,
+    "Nighthawk Royal Guards": 3497000,
+    "Nighthawk Vanguards": 3612873
+}
+
+nighthawk_group_ranks = {
+    # Nighthawk Combat Engineers
+    4809530: [
+        "| N | Servitor",
+        "| T | Explorator",
+        "| T | Secutor",
+        "| T | Guard",
+        "| T | Knight",
+        "| T | Titan",
+        "| O | Moderatus",
+        "| O | Dominatus",
+        "| O | Magos",
+        "| HC | Ordinatus",
+        "| HC | Praefectus",
+        "| X | Bot",
+        "| X | Developer",
+        "| X | Fabricator-General",
+        "| X | Overseer",
+        "| X | Administration",
+        "| X | Viceroy"
+    ],
+    # Nighthawk Combat Engineers: Leviathan
+    33391710: [
+        "| N | Apprentice",
+        "| T | Neophyte",
+        "| T | Acolyte",
+        "| T | Mage",
+        "| O | Preceptor",
+        "| O | Seer",
+        "| A | Elder",
+        "- | Ordinatus",
+        "- | Developer",
+        "| HC | Oracle",
+        "| X | Archon",
+        "| X | Praefectus",
+        "| X | Fabricator-General",
+        "| X | Overseer",
+        "| X | Administration",
+        "| X | Viceroy"
+    ],
+    # Nighthawk Commandos
+    3496996: [
+        "Probationary Trooper",
+        "Commando",
+        "Sentinel",
+        "Infiltrator",
+        "Operative",
+        "Specialist",
+        "Prestige",
+        "Nighthawk Nine",
+        "Legend",
+        "Interim Warrant Officer",
+        "Warrant Officer",
+        "Chief Warrant Officer",
+        "Captain",
+        "Commandant",
+        "Advisor",
+        "Deputy Director",
+        "Developer",
+        "Director",
+        "Overseer",
+        "Administration",
+        "Commander",
+        "Viceroy"
+    ],
+    # Nighthawk Commandos: Ghosts
+    11000162: [
+        "Ghost Trial",
+        "Ghost Operative",
+        "Cheerleader",
+        "Ghost Senior Operative",
+        "Ghost Grenadier",
+        "Ghost Specialist",
+        "Nighthawk Nine",
+        "Ghost Sergeant",
+        "Ghost Squad Leader",
+        "Ghost Overseer",
+        "Ghost Commandant",
+        "Ghost Commander",
+        "Commando Administration",
+        "Overseer",
+        "Developer",
+        "Administration",
+        "Commander",
+        "Viceroy"
+    ],
+    # Nighthawk Guardians
+    4183818: [
+        "Trial",
+        "Guardian",
+        "Gladius",
+        "Sentinel",
+        "Centurion",
+        "Champion",
+        "Olympian",
+        "Imperial",
+        "Senate",
+        "Council",
+        "Warden",
+        "Consul",
+        "Developer",
+        "Forerunner",
+        "Overseer",
+        "Administration",
+        "Viceroy"
+    ],
+    # Nighthawk Imperial Peacekeeper Corps
+    4543661: [
+        "Private",
+        "Corporal",
+        "Sergeant",
+        "Lieutenant",
+        "Captain",
+        "Advisory",
+        "Commander",
+        "Administration",
+        "Imperial Primarch",
+        "Viceroy"
+    ],
+    # Nighthawk Imperium
+    1174414: [
+        "| 1 | Citizen",
+        "| 2 | Enlist",
+        "| 3 | Cadet",
+        "| T | Trooper",
+        "| T | Private",
+        "| T | Specialist",
+        "| T | Corporal",
+        "| T | Sergeant",
+        "| T | Staff Sergeant",
+        "| N | Sergeant First Class",
+        "| N | Master Sergeant",
+        "| N | Sergeant Major",
+        "| E | Ensign",
+        "| SLT | Second Lieutenant",
+        "| LT | First Lieutenant",
+        "| O | Captain",
+        "| O | Colonel",
+        "| C | Commodore",
+        "| HC | Admiral",
+        "--- Contractor ---",
+        "--- Developer ---",
+        "| HC | Arch Admiral",
+        "| X | Supreme Admiral",
+        "| X | Commander",
+        "| X | Viceroy"
+    ],
+    # Nighthawk Integration Department
+    8244715: [
+        "Trial",
+        "Junior Member",
+        "Senior Member",
+        "Department Member",
+        "Colonel",
+        "Chief of Integration",
+        "Administration",
+        "Commander",
+        "Viceroy"
+    ],
+    # Nighthawk Manticore
+    15026315: [
+        "Probationary Member",
+        "Cadet",
+        "Agent",
+        "Operative",
+        "Senior Operative",
+        "Elite",
+        "Auxilior",
+        "Zealot",
+        "Kaidon",
+        "-",
+        "Chief Superintendent",
+        "Arbiter",
+        "Deputy Chief",
+        "Chief",
+        "Administration",
+        "Viceroy"
+    ],
+    # Nighthawk Military Police
+    3497030: [
+        "| N | Enlist/Suspended",
+        "| L | Probationary Cadet",
+        "| L | Senior Cadet",
+        "| L | Enforcer",
+        "| M | Senior Enforcer",
+        "| M | Marshal",
+        "| M | Sergeant",
+        "| M | Constable",
+        "| H | Inspector",
+        "| H | Superintendent",
+        "| HC | Chief Superintendent",
+        "| HC | Deputy Chief",
+        "| X | Developer",
+        "| C | Chief",
+        "| X | Overseer",
+        "| X | Commander",
+        "| X | Administration",
+        "| X | Viceroy"
+    ],
+    # Nighthawk Military Police: Cerberus
+    4486074: [
+        "| N | Novice",
+        "| T | Specialist",
+        "| T | Tactician",
+        "| T | Sergeant",
+        "| T | Master Sergeant",
+        "| O | Drill Sergeant",
+        "| O | Ultra",
+        "| C | Council",
+        "| C | Captain-Major",
+        "-",
+        "| X | Chief Superintendent",
+        "| X | Commandant",
+        "| X | Deputy Chief",
+        "| X | Chief",
+        "| X | Overseer",
+        "| X | Administration",
+        "| X | Commander",
+        "| X | Viceroy"
+    ],
+    # Nighthawk Reaper Battalion
+    4734688: [
+        "Recruit",
+        "Novice",
+        "Adept",
+        "Sentry",
+        "Protector",
+        "Guide",
+        "Herald",
+        "Harbinger",
+        "Prime",
+        "Maven",
+        "Arbiter",
+        "Warden",
+        "Developer",
+        "Director",
+        "Overseer",
+        "Commander",
+        "Administration",
+        "Viceroy"
+    ],
+    # Nighthawk Reaper Battalion: Black Daggers
+    4936035: [
+        "Initiate",
+        "Justiciar",
+        "Knight",
+        "Paladin",
+        "Senior Paladin",
+        "Sentinel",
+        "Praetorian",
+        "Imperator",
+        "Praefectus",
+        "General",
+        "Director",
+        "Developer",
+        "Overseer",
+        "Administration",
+        "Viceroy"
+    ],
+    # Nighthawk Royal Guards
+    3497000: [
+        "Trial",
+        "Knight",
+        "Vindicator",
+        "Sentinel",
+        "Champion",
+        "Royal Officer",
+        "High Command",
+        "Developer",
+        "Director",
+        "Overseer",
+        "Administration",
+        "Commander",
+        "Viceroy"
+    ],
+    # Nighthawk Vanguards
+    3612873: [
+        "Citizen",
+        "L | Squire",
+        "L | Gladiator",
+        "L | Knight",
+        "M | Vanguard",
+        "M | Champion",
+        "M | Blademaster",
+        "M | Legionary",
+        "H | Warlord",
+        "H | Praetorian",
+        "C | Council",
+        "C | Consul",
+        "X | Developer",
+        "Forerunner",
+        "Overseer",
+        "Administration",
+        "Commander",
+        "Viceroy"
+    ]
+}
+'''
+
 import requests
 
-target_users = ["41uis", "Expiral"]
+target_users = ["41uis", "Expiral", "daxlovescars", 'Acrynax', 'finalnickADD', 'Kurashina_lzumi', 'vTakina', 'Te0Void', 'Tim_NightShade', 'Caonalyst', 'snellejelte', 'Guy_Broman', 'ReiAstra', 'flem_sy', 'JBF3', 'KolegaPolakYT', 'Pieface1091']
 
 groups_of_interest = {
-    "Cerberus": 4486074,
+    "Nighthawk Combat Engineers: Leviathan": 33391710,
+    "Nighthawk Imperial Peacekeeper Corps": 4543661
 }
 
 roles_of_interest = {
+    # Nighthawk Military Police: Cerberus
     4486074: [
         "| O | Drill Sergeant",
         "| O | Ultra",
@@ -20,6 +344,95 @@ roles_of_interest = {
         "| X | Administration",
         "| X | Commander",
         "| X | Viceroy"
+    ],
+      # Nighthawk Manticore
+    15026315: [
+        "Operative",
+        "Senior Operative",
+        "Elite",
+        "Auxilior",
+        "Zealot",
+        "Kaidon",
+        "Chief Superintendent",
+        "Arbiter",
+        "Deputy Chief",
+        "Chief",
+        "Administration",
+        "Viceroy"
+    ],
+    # Nighthawk Imperium
+    1174414: [
+        "| C | Commodore",
+        "| HC | Admiral",
+        "| HC | Arch Admiral",
+        "| X | Supreme Admiral",
+        "| X | Commander",
+        "| X | Viceroy"
+    ],
+    # Nighthawk Royal Guards
+    3497000: [
+        "Royal Officer",
+        "High Command",
+        "Developer",
+        "Director",
+        "Overseer",
+        "Administration",
+        "Commander",
+        "Viceroy"
+    ],
+    # Nighthawk Combat Engineers
+    4809530: [
+        "| HC | Ordinatus",
+        "| HC | Praefectus",
+        "| X | Fabricator-General",
+        "| X | Overseer",
+        "| X | Administration",
+        "| X | Viceroy"
+    ],
+    # Nighthawk Commandos
+    3496996: [
+        "Commandant",
+        "Advisor",
+        "Deputy Director",
+        "Developer",
+        "Director",
+        "Overseer",
+        "Administration",
+        "Commander",
+        "Viceroy"
+    ],
+    # Nighthawk Military Police
+    3497030: [
+        "| HC | Chief Superintendent",
+        "| HC | Deputy Chief",
+        "| X | Developer",
+        "| C | Chief",
+        "| X | Overseer",
+        "| X | Commander",
+        "| X | Administration",
+        "| X | Viceroy"
+    ],
+    # Nighthawk Reaper Battalion
+    4734688: [
+        "Arbiter",
+        "Warden",
+        "Developer",
+        "Director",
+        "Overseer",
+        "Commander",
+        "Administration",
+        "Viceroy"
+    ],
+    # Nighthawk Vanguards
+    3612873: [
+        "C | Council",
+        "C | Consul",
+        "X | Developer",
+        "Forerunner",
+        "Overseer",
+        "Administration",
+        "Commander",
+        "Viceroy"
     ]
 }
 
@@ -27,26 +440,26 @@ def search_users(keyword):
     print("-" * 40)
     url = f"https://users.roblox.com/v1/users/search?keyword={keyword}&limit=100"
     response = requests.get(url)
-    
+
     if response.status_code == 200:
         data = response.json()
         users = data.get('data', [])
         found_user = False
-        
+
         for user in users:
             if user['name'].lower() == keyword.lower():
                 found_user = True
                 print(f"Username: {user['name']}, DisplayName: {user['displayName']}, Id: {user['id']}")
                 print("-" * 40)
                 is_target = False
-                
+
                 if user['name'].lower() in [u.lower() for u in target_users]:
                     print(f"{user['name']}, Permanent Hit")
                     is_target = True
-                
+
                 fetch_user_groups(user['id'], user['name'], is_target)
                 break
-        
+
         if not found_user:
             print(f"No exact match found for username: {keyword}")
 
@@ -56,16 +469,16 @@ def search_users(keyword):
 def fetch_user_groups(user_id, username, is_target):
     url = f"https://groups.roblox.com/v1/users/{user_id}/groups/roles"
     response = requests.get(url)
-    
+
     if response.status_code == 200:
         data = response.json()
         groups = data.get('data', [])
-        
+
         for group in groups:
             group_id = group['group']['id']
             group_name = group['group']['name']
             role_name = group['role']['name']
-            
+
             if group_id in groups_of_interest.values():
                 print(f"{group_name}, {username} | Permanent Hit")
                 is_target = True
@@ -73,7 +486,7 @@ def fetch_user_groups(user_id, username, is_target):
             if group_id in roles_of_interest and role_name in roles_of_interest[group_id]:
                 print(f"{group_name}, {role_name}, {username} | Permanent Hit")
                 is_target = True
-        
+
         if not is_target:
             print(f"No target roles or groups of interest found for User ID {user_id}.")
     else:
